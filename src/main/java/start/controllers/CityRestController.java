5f10cf45901f8925ff9490dc66a8048003029810
Hariton-Andrei-Marius <Andrei.Hariton@scai-consulting.grupposcai.it>
2018-02-15 16:57:49 +0100

    Basic inside-app REST API @ "/rest/city?name=CityName"

--- src/main/java/start/controllers/CityRestController.java
+++ src/main/java/start/controllers/CityRestController.java
@@ -2,6 +2,7 @@ package start.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,16 +11,17 @@ import objects.*;
 @RestController
 public class CityRestController {
 	
-	private static final String REST_API = "http://openweathermap.org/data/2.5/weather?appid=b6907d289e10d714a6e88b30761fae22&q=Bologna,it";
+	private static final String REST_API = "http://openweathermap.org/data/2.5/weather?appid=b6907d289e10d714a6e88b30761fae22&q=";
 	
-    @RequestMapping("/rest/city")
-    public City city(Model model) {
-    	
-    	RestTemplate restTemplate = new RestTemplate();
+	@RequestMapping("/rest/city")
+	public City city(Model model,
+		@RequestParam(value = "name", required = false, defaultValue = "Bologna,it") String name)
+	{
+		RestTemplate restTemplate = new RestTemplate();
         
-        City city = restTemplate.getForObject(REST_API, City.class);
+		City city = restTemplate.getForObject(REST_API + name, City.class);
     	
-        return city;
-    }
+		return city;
+	}
 
 }
