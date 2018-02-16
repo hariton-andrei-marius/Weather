847dfa0751d715ec5c10fd04751761e04afe09f8
Hariton-Andrei-Marius <Andrei.Hariton@scai-consulting.grupposcai.it>
2018-02-16 17:12:57 +0100

    Added normal controllers

---
+++ src/main/java/start/controllers/CityDetailsController.java
@@ -0,0 +1,38 @@
+package start.controllers;
+
+import java.net.URISyntaxException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import start.modules.RestApi;
+
+@Controller
+public class CityDetailsController {
+	
+	@RequestMapping("/city_details")
+	public String weather(Model model,
+
+		@RequestParam(value = "city", required = false, defaultValue = "Bologna") String city)
+	{
+		Object results = null;
+		
+		try
+		{
+			results = new RestTemplate().getForObject(RestApi.getCityDetailsURI(city), Object.class);
+		}
+		catch (RestClientException | URISyntaxException e)
+		{
+			results = e.getMessage();
+		}
+		
+		model.addAttribute("results", results);
+    	
+		return "city_details";
+	}
+
+}
